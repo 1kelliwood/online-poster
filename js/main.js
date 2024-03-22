5c4b8d06f32453887e667a73587bfb605b4a9973
1kelliwood <1kelli.wood@gmail.com>
2024-03-22 19:00:36 -0400

    Updated CSS Design

--- js/main.js
+++ js/main.js
@@ -1,4 +1,31 @@
 $(document).ready(() => {
 
+    $("#show-speakers").click(function(){
+    $("#introduction").hide();
+    $("#speakers").show(); 
+    });
+
+   $("#anther_kiley_dot").click(function(){
+        $("#anther_kiley h3").toggle();
+        $("#anther_kiley .event_details").toggle();
+    });
+
+    $("#takahashi_kuan_dot").click(function(){
+        $("#takahashi_kuan h3").toggle();
+        $("#takahashi_kuan .event_details").toggle();
+    });
+
+    $("#e_roon_kang_dot").click(function(){
+        $("#e_roon_kang h3").toggle();
+        $("#e_roon_kang .event_details").toggle();
+    });
+
+      $("#sebastian_aubin").click(function(){
+        $("#sebastian_aubin h3").toggle();
+        $("#sebastian_aubin .event_details").toggle();
+    });
+
 
 });
+
+
